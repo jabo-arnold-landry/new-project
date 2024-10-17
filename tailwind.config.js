f7f4109589f9735810e77664503cff589dc7f598
jabo arnold landry <arnoldjabo@gmail.com>
2024-10-16 20:08:14 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      zIndex: {
+        '1':'1'
+      }
+    },
+  },
+  plugins: [],
+}
+
